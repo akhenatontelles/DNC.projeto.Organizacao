fcb86d005e624fa4f16d3c92f7ff4c9b3e92b49a
Akhenaton Telles Claussen <37838938+akhenatontelles@users.noreply.github.com>
2024-01-17 03:13:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,168 +1,166 @@
-@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,300&display=swap');
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import './App.css';
 
-.App {
- font-family: Arial, sans-serif;
- width: 100%;
+import { ImBin2 } from "react-icons/im";
+import { RiPencilFill } from "react-icons/ri";
+import { FaPlus } from "react-icons/fa";
+
+Modal.setAppElement('#root');
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [selectedItemId, setSelectedItemId] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isInputVisible, setIsInputVisible] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
 
  
+  const [description, setDescription] = useState('');
+
+  function addItem(e) {
+    e.preventDefault();
+    const itemText = e.target.itemInput.value.trim();
+
+    if (itemText !== '') {
+      setItems([...items, { id: items.length + 1, text: itemText, description }]);
+      e.target.itemInput.value = '';
+      setDescription('');
+      setIsInputVisible(false);
+    }
+  }
+
+  function deleteItem(id) {
+    setSelectedItemId(id);
+    setIsDeleteModalOpen(true);
+  }
+
+  function confirmDelete() {
+    setItems(items.filter(item => item.id !== selectedItemId));
+    setSelectedItems(selectedItems.filter(selectedId => selectedId !== selectedItemId));
+    setIsDeleteModalOpen(false);
+  }
+
+  function toggleSelectItem(id) {
+    setSelectedItems(prevSelectedItems => {
+      if (prevSelectedItems.includes(id)) {
+        return prevSelectedItems.filter(selectedId => selectedId !== id);
+      } else {
+        return [...prevSelectedItems, id];
+      }
+    });
+  }
+
+  function openModal(id) {
+    setSelectedItemId(id);
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setSelectedItemId(null);
+    setIsModalOpen(false);
+  }
+
+  function closeDeleteModal() {
+    setSelectedItemId(null);
+    setIsDeleteModalOpen(false);
+  }
+
+  return (
+    <div className="App">
+      <div className='topo'>
+     
+                <button className='organizacao'>Organização</button>
+                <button className='tarefas'>Tarefas</button>
+           
+      
+      </div>
+      
+      <div className='container'>
+        <div  className="titulo"><span>Otimize seu tempo e se organize com o nosso Planejador Diário.</span></div>
+        <div className="flex-container">
+          <div className="grid-item">Tarefa</div>
+          <div className="grid-item">Status</div>
+          <div className="grid-item">Opções</div>
+        </div>
+        <ul>
+          <div className='tabela'>
+            {items.map(item => (
+              <li key={item.id}>
+                <span onClick={() => openModal(item.id)} style={{ cursor: 'pointer' }}>
+                  {item.text}
+                </span>
+                <div>
+                  <input
+                    className='status'
+                    type="checkbox"
+                    checked={selectedItems.includes(item.id)}
+                    onChange={() => toggleSelectItem(item.id)}
+                  />
+                  <button className='botao' onClick={() => openModal(item.id)}><RiPencilFill /></button>
+                  <button className='botao' onClick={() => deleteItem(item.id)}><ImBin2 /></button>
+                </div>
+              </li>
+            ))}
+          </div>
+        </ul>
+        <span className='adicionar2'>Novo Tarefa</span>
+        <button className='adicionar' onClick={() => setIsInputVisible(true)}><FaPlus /></button>
+        {isInputVisible && (
+          <form onSubmit={addItem}>
+            <input type="text" name="itemInput" placeholder="Digite um item..." />
+            <input type="text" name="descriptionInput" placeholder="Digite uma descrição..." value={description} onChange={(e) => setDescription(e.target.value)} />
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
+
+        
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          contentLabel="Detalhes do Item"
+          className="react-modal"
+          overlayClassName="react-modal-overlay"
+        >
+          {selectedItemId && (
+            <div>
+              <button className="react-modal-close-button" onClick={closeModal}>&times;</button>
+              <h2 className='detalhes'>Deseja editar esse item?</h2>
+              <p>ID: {selectedItemId}</p>
+              <p className='botoes'>{items.find(item => item.id === selectedItemId)?.description}</p>
+              <div className='botoes'>
+                <button className='nao' onClick={closeModal}>Não</button>
+                <button className='sim'>Sim</button>
+              </div>
+            </div>
+          )}
+        </Modal>
+
+       
+        <Modal
+          isOpen={isDeleteModalOpen}
+          onRequestClose={closeDeleteModal}
+          contentLabel="Confirmar Exclusão"
+          className="react-modal"
+          overlayClassName="react-modal-overlay"
+        >
+          {selectedItemId && (
+            <div>
+              <button className="react-modal-close-button" onClick={closeDeleteModal}>&times;</button>
+              <h2 className='detalhes'>Deseja excluir este item?</h2>
+              <p>ID: {selectedItemId}</p>
+              <p className='botoes'>{items.find(item => item.id === selectedItemId)?.description}</p>
+              <div className='botoes'>
+                <button className='nao' onClick={closeDeleteModal}>Não</button>
+                <button className='sim' onClick={confirmDelete}>Sim</button>
+              </div>
+            </div>
+          )}
+        </Modal>
+      </div>
+    </div>
+  );
 }
 
-form {
- display: flex;
- margin-bottom: 1em;
-}
-
-input[type="text"] {
- flex: 1;
- padding: 0.5em;
-}
-
-
-
-ul {
- list-style-type: none;
- padding: 0;
-}
-
-li {
- padding: 0.5em 0;
- display: flex;
- justify-content: space-between;
- align-items: center;
- display: flex;
-}
-
-li button {
- cursor: pointer;
-}
-
-.modal_cor{
-  color: black;
-}
-.status{
-  float: left;
-  margin-left: -250px;
-}
-.flex-container {
-  display: flex;
-  margin-top: 100px;
-  width: 600px;
-  justify-content: space-between;
-}
-
-.grid-item {
-  flex: 1 0 15.33%; 
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  padding: 10px;
-  box-sizing: border-box; 
-  border-bottom: 3px solid white ;
-  text-align: right;
-  
-}
-.botao{
-  float: left;
-}
-.titulo{
-  font-size: 25px;
-  font-weight: 600;
-  text-align: center;
-  margin-left: -120px;
-  width: 800px;
-
-}
-.adicionar{
-float: right;
-font-family: 'Cookie', cursive;
-}
-.adicionar2{
-  font-family: 'Raleway', sans-serif;
-  color: #a5acb3;
-  }
-  .react-modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 500px;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 5px;
-    color: #000;
-  }
-  
-
-  .react-modal-close-button {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 16px;
-    cursor: pointer;
-    color: #000;
-  }
-  .topo{
-    background-color: #0C70F2;
-    width: 101%;
-    height: 54px;
-    margin-top: -10px;
-    margin-left: -10px;
-  }
-  .container{
-    width: 600px;
-    height: 400px;
-    margin: 0 auto;
-    margin-top: 150px;
-    
-  }
-  .tabela{
-    width:600px;
-  }
-  .nao{
-  background-color: #0C70F2;
-  color: white;
-  padding: 10px 15px;
-  border-radius: 5px;
-  cursor: pointer;
-  padding: 10px 50px 10px 50px;
-  }
-  .nao:hover {
-    background-color: #094faa;
-  }
-  .sim {
-    background-color: #ffffff;
-    color: rgb(48, 48, 48);
-    padding: 10px 15px;
-    border-radius: 5px;
-    cursor: pointer;
-    border: 1px solid black;
-    margin: 5px;
-    padding: 10px 50px 10px 50px;
-  }
-  .sim:hover {
-    background-color: #ffffff;  
-  }
-  .tarefas {
-    background-color: #ffffff;
-    color: #0C70F2;
-    padding: 10px 15px;
-    border-radius: 10px 10px 00px 00px;
-    cursor: pointer;
-    margin: 5px;
-    padding: 10px 30px 10px 30px;
-    font-weight: 600;
-    margin-top: 14px;
-  }
-  .organizacao{
-    font-weight: 600;
-  }
-  
-  
-  .botoes{
-    text-align: center;
-  }
-  .detalhes{
-    text-align: center;
-  }
-  .botoes_topo{
-    text-align: left;
-  }
+export default App;
